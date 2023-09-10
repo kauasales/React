fa30e9caf5213086d805d92581749c98acc49051
Kauã Sales <119974952+kauasales@users.noreply.github.com>
2023-09-10 17:09:32 -0400

    Add files via upload

---
+++ Q26arrow.js
@@ -0,0 +1,20 @@
+let anosEstudo = 4
+
+let experiencia = (x) => {
+
+    if(x <= 1){
+        return window.alert("Junior")
+    } 
+
+    else if(x <= 3){
+        return window.alert("Pleno")
+    }
+
+    else if(x <= 6){
+        return window.alert("Senior")
+    }
+
+    else if(6 < x){
+        return window.alert("Grão Mestre Jedi")
+    }
+}
